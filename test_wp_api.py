6521ef80064d56a3e8621c6cc07692eda6db7776
grahanoi <grahanoi@students.zhaw.ch>
2026-06-29 10:02:40 +0200

    add order

--- test_wp_api.py
+++ test_wp_api.py
@@ -1,8 +1,8 @@
 from wp_api import WPClient
 
-client = WPClient(base_url="https://imaginatic.es")
+client = WPClient(base_url="https://wptavern.com/")
 
-# List published posts, number of post here 5, chose page 1 or 2
-posts = client.posts.list(status="publish", per_page=5, page=2)
+# List published posts, number of post here 5, chose page 1 or 2, orderby
+posts = client.posts.list(status="publish", per_page=5, page=1, orderby="date")
 for post in posts:
     print(post['id'], post['title']['rendered'])
