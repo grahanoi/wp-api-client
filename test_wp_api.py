3908f40bbf2e0b7605e8a6e96034675ce9afb688
grahanoi <grahanoi@students.zhaw.ch>
2026-06-29 10:30:59 +0200

    use same structure as issue form

--- test_wp_api.py
+++ test_wp_api.py
@@ -1,8 +1,58 @@
-from wp_api import WPClient
+"""from wp_api import WPClient
 
-client = WPClient(base_url="https://wptavern.com/")
+client = WPClient(base_url="https://imaginatic.es")
 
 # List published posts, number of post here 5, chose page 1 or 2, orderby
-posts = client.posts.list(status="publish", per_page=5, page=1, orderby="date")
+posts = client.posts.list(status="publish", per_page=1, page=1, orderby="date")
 for post in posts:
-    print(post['id'], post['title']['rendered'])
+    print(post['id'], post['title']['rendered'], post['date'][:10], post['content']['rendered'], post['link'], post['author'])  # Print first 100 characters of content """
+
+from wp_api import WPClient
+from html.parser import HTMLParser
+import re
+
+# Simple HTML stripper
+class HTMLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.text = []
+    def handle_data(self, data):
+        self.text.append(data)
+    def get_text(self):
+        return ''.join(self.text).strip()
+
+def strip_html(html):
+    stripper = HTMLStripper()
+    stripper.feed(html)
+    return stripper.get_text()
+
+client = WPClient(base_url="https://imaginatic.es")
+posts = client.posts.list(status="publish", per_page=5, page=1, orderby="date")
+
+for post in posts:
+    post_id   = post['id']
+    title     = post['title']['rendered']
+    date      = post['date'][:10]  # just the YYYY-MM-DD part
+    content   = strip_html(post['content']['rendered'])
+    link      = post['link']
+
+    # Fetch author name
+    author_id = post['author']
+    author    = client.users.get(author_id)
+    author_name = author['name']
+
+    yaml = f"""---
+title: {title}
+author: {author_name}
+date: {date}
+type: news
+tags:
+- news
+link: {link}
+description: |
+  {content[:200]}...
+---
+{content}
+"""
+    print(yaml)
+    print("---")
